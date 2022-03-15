15ebf6c74f850f215dce91e52fb0f54468605ccf
Rubdh <benrhu@gmail.com>
2022-03-15 14:09:38 +0100

    // 2nd update

---
+++ Js/editor.js
@@ -0,0 +1,46 @@
+const blogTitle = document.querySelector('.title');
+const article = document.querySelector('.article')
+
+const img = document.querySelector('.img');
+const imgUpload = document.querySelector('.imgInput');
+let imgPath;
+const publishBtn = document.querySelector('.publish-btn');
+const uploadInput = document.querySelector('.imgUpload');
+
+img.addEventListener('change', () => {
+    uploadImage(img, "imgUpload");
+    })
+
+uploadInput.addEventListener('change', () => {
+    uploadImage(img, "image");
+})
+
+const uploadImage = (uploadFile, uploadType) => {
+    const [file] = uploadFile.files;
+    if(file && file.type.includes("image")){
+        const formdata = new FormData();
+        formdata.append('image', file)
+
+        fetch('/upload', {
+            method: 'POST',
+            body: formdata
+        }).then(res => res.json())
+        .then(data => {
+            if(uploadType == "image"){
+                addImage(data, file.name);
+            }else{
+            imgPath = `${location.origin}/${data}`;
+            imgUpload.style.backgroundImage = `url("${imgPath}")`}
+            })
+    } else{ alert("Upload image only"); }
+}
+
+const addImage =(pathImage, alt) =>{
+    let pos = article.selectionStart;
+    let insertText = `\r ![${alt}] (${pathImage}) \r`;
+    article.value = article.value.slice(0, pos)+insertText + article.value.slice(pos);
+}
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+publishBtn
--- Js/server.js
+++ Js/server.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const path = require('path');
+const fileupload = require('express-fileupload');
 
 let initial_path = path.join(__dirname, "../");
 
 const app = express();
 app.use(express.static(initial_path))
+app.use(fileupload())
 
-app.get('/', (req, res) => { res.sendFile(path.join(initial_path, 'home.html')); })
+/** Routes
+ * -> Route for Home
+ * -> Route for Editor
+ * -> Route for Upload
+ */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, 'home.html'));
+})
 
-app.get('/editor', (req, res) => { res.sendFile(path.join(initial_path, 'editor.html'))})
+app.get('/editor', (req, res) => {
+    res.sendFile(path.join(initial_path, 'editor.html'))
+})
+
+// Upload links
+
+app.post('/upload',(req, res) => {
+    let file = req.files.image;
+    let path = 'BlogJS/uploads' + imageName;
+    let imageName = date.getDate + date.getTime + file.name;
+    let date = new Date();
+    file.mv(path, (err, result) => {
+        if (err) {
+            throw err;
+        }else{
+            res.json(`uploads/${imageName}`)
+        }
+    }
+)
+
+})
 
 app.listen("3000", () => {
     console.log('Listening')
