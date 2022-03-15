24e0b7e8b8bece99ba4f0c0550de8db99c16eea3
Rubdh <benrhu@gmail.com>
2022-03-15 16:33:36 +0100

    Install prettier and more changes

--- public/Js/server.js
+++ public/Js/server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const path = require('path');
 const fileupload = require('express-fileupload');
 
-let initial_path = path.join(__dirname, "../");
-
 const app = express();
-app.use(express.static(initial_path))
-app.use(fileupload())
+app.use(express.static("public"));
+app.use(fileupload());
 
 /** Routes
  * -> Route for Home
@@ -14,11 +12,11 @@ app.use(fileupload())
  * -> Route for Upload
  */
 app.get('/', (req, res) => {
-    res.sendFile(path.join(initial_path, 'home.html'));
+    res.sendFile('home.html');
 })
 
 app.get('/editor', (req, res) => {
-    res.sendFile(path.join(initial_path, 'editor.html'))
+    res.sendFile( 'editor.html');
 })
 
 // Upload links
